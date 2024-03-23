80b9604ed1118bde5106bc32795dde25ae84999e
jahansher333 <160265854+jahansher333@users.noreply.github.com>
2024-03-23 20:43:11 +0000

    push my CLI GUESSING NUMBER

---
+++ README.md
@@ -0,0 +1,1 @@
+"# CLI---GUESSING--NUMBER--" 
---
+++ index.ts
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+
+
+const randomNumber = Math.floor(Math.random() * 10 + 1 );
+  
+
+const answer = await inquirer.prompt([
+    {
+    name:"userGuessNumber",
+    type: "number",
+    message: "pleas guess a Number between 1 to 10",
+
+ },
+]);
+ 
+
+  if (answer . userGuessNumber === randomNumber) {
+    console.log("Congratulation! you can guest Number.");
+  }else{
+    console.log("Lose Game , You Are Wroung Number");
+  }
